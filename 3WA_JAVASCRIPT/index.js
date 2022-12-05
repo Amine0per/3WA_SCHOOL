bd8e2ef98574a89c0b41c7ef75a3f84de42197ec
Amine <amine.oubrik@gmail.com>
2022-12-05 21:32:22 +0100

    Update index.js

--- 3WA_JAVASCRIPT/index.js
+++ 3WA_JAVASCRIPT/index.js
@@ -1,6 +1,53 @@
-let longueur =15;
 
-longueur=45;
+// declaration variable et constante
 
-console.log(longueur);
+let nom = "Amine" ;
+const age = 30 ;
+ 
+
+// affichage sur console
+console. log (nom + " a " + age + " ans ") ;  
+
+
+
+
+// modele litteraux
+const ville = "Marrakech" ;
+console.log ("j'habite dans la ville de " + ville) ;
+
+
+
+
+// interpolation de chaines
+console.log (`je suis en train
+    de faire
+            un test d'interpolation de chaines
+                 tout simplement`);
+
+
+const restaurant = "kfc" ;
+console.log(`Mon restaurant favori est : ${restaurant} !`);
+
+
+// iUpLowerCase
+const cafe = "Coffee Shop" ;
+console.log(cafe.toLowerCase() );
+console.log(cafe.toUpperCase() );
+
+//repeat
+const Messi = "vite";
+console.log("messi cour" + Messi.repeat(5) );
+
+
+//remplacer
+const maison = "ma maison a la couleur rouge";
+const newmaison = maison.replace("rouge" , "bleu");
+
+console.log (maison);
+console.log (newmaison);
+
+
+//rechercher
+console.log (maison.includes("rouge"));
+console.log (maison.includes("noir"));
 
