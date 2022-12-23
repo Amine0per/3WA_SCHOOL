d68d65a2a6e91962d3944e984461db49dfe6f751
Amine <amine.oubrik@gmail.com>
2022-12-23 21:14:33 +0100

    course 23/12/2022

--- 3WA_JAVASCRIPT/index3.js
+++ 3WA_JAVASCRIPT/index3.js
@@ -108,3 +108,78 @@ console.log("l'aire du cercle dont le rayon est "+ rayon+ " est : " +aire );
 const hello=()=>{
     console.log ("bonjour tout le monde");
 }
+
+
+//table
+let boire = [22, 16, 14, 2 ,5 ,7];
+console.log (boire[4]); // afficher le contenu de lindice 4 01234...
+console.log (boire.length);
+
+for (let i=0; i<boire.length; i++){
+console.log(boire[i]);}
+
+
+//afficher tableau
+boire.forEach((boire)=>{
+    console.log(boire);
+});
+
+//affectation cellule tableau
+boire[2]="tea"
+console.log(boire);
+
+// trier sort 
+let trieboire = boire.sort();
+console.log (trieboire);
+boire.sort(function(a,b){
+    return a-b; // a-b croissant b-a decroissant
+});
+console.log (boire);
+
+// ajouter cellule
+boire.push("jus d'orange");
+console.log(boire);
+
+//supprimer dernier element uniquement
+boire.pop();
+console.log(boire);
+
+//inverser le tableau
+boire.reverse();
+console.log(boire);
+
+//supprimer la 1er valeur et la garder 
+let supprimer = boire.shift();
+console.log(supprimer);
+console.log(boire);
+
+//ajoutter element au debut avec unshift
+let nouveau = boire.unshift("jus citron","jus ananas");
+console.log(nouveau); //affiche taille du tableau incluant les nouveaux elements ajoutees
+console.log(boire);
+
+//concatenation de tableau (rassembler 2 tableau en 1 ou 3 en 1 dans ce cas)
+let plat = ["couscouse", "tajine", "riz"];
+let messi=["couscouse", "tajines", "rizs"];
+let totaltableau= boire.concat(plat,messi);
+console.log(totaltableau); 
+
+//prendre une tranche du tableau dans ce cas la tranche entre lindice 1 et 4 
+let essay = boire.slice(1,4);
+console.log(essay);
+
+//affiche lindex de la cellule du tableau dans notre cas jus ananas est dans la cellule 1
+let trouver=boire.indexOf("jus ananas");
+console.log(trouver);
+
+//.includes verifie si un element existe dans le  tableau retoune true ou false
+let a=boire.includes("jus ananas");
+console.log(a);
+
+//somme des elements du tableau 
+let number=[1,2,3,4,5,6,7,8];
+let total = number.reduce(function(total,a){
+
+    return total + a;
+})
+console.log(total);
