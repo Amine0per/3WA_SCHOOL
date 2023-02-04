caccc072aeffa83e065977914640ea25209c60b4
Amine <amine.oubrik@gmail.com>
2023-02-04 10:58:28 +0100

    chatbox

--- 3WA_JQUERY/ChatBox.js
+++ 3WA_JQUERY/ChatBox.js
@@ -1,15 +1,16 @@
  $(document).ready(function(){
 
         $('form').submit(function(e){   // declenchement d'une fonction sur submit du formulaire                  
-          e.preventDefault(); // eviter que la page se recharge             
+          e.preventDefault();// eviter que la page se recharge             
           let MessageUtilisateur = $('input').val(); // obtention du message         
-          if( MessageUtilisateur !== '' )  // verification si le message est vide 
+          if( MessageUtilisateur !== '' )  // verification si le message n'est pas vide 
           {     
             // Preparation du message
             let NouveauMessage = "<p><strong>Utilisateur :</strong> " + MessageUtilisateur + "</p>"; 
       
             // Appel de la fonction qui gere lajout du message et la reponse du bot en callback
             Ajout_Message_Et_Reponse_Bot_En_CallBack( MessageUtilisateur , NouveauMessage  ,Reponse_Du_Bot );                 
+            
             $('input').val(""); // nettoyage de l'input           
           } 
           else { alert("Veuillez ecrire quelque chose dans le champ");}  // alert de l'utilisateur si il entre un champ vide
@@ -26,7 +27,7 @@
              //les differents cas de reponse du BOT
               switch(r){   
 
-                case "Bonjour" :$('.LeBox').append("<p class=' message '> <strong>Bot : </strong>  Bonjour, comment vas tu  </p>"); break ;
+                case "bonjour" :$('.LeBox').append("<p class=' message '> <strong>Bot : </strong>  Bonjour, comment vas tu  </p>"); break ;
                 case  "ca va et toi" : $('.LeBox').append("<p class=' message '> <strong>Bot : </strong>  je vais bien merci  </p>");break ;
                 case  "tu fais quoi" : $('.LeBox').append("<p class=' message '> <strong>Bot : </strong>  je regarde une nouvelle serie </p>");break ;
                 case  "quel serie" : $('.LeBox').append("<p class=' message bg-warning'> <strong>Bot : </strong> Last of us </p>");break ;
